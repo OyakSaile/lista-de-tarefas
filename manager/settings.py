3d0d916fcc2f82a438441c74c32c4d9f536df1fa
mauricioDosantos <mauriciodossantoscosta5@gmail.com>
2020-12-29 09:53:20 -0300

    Criação do modelo de banco de dados

--- manager/settings.py
+++ manager/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, PurePath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'to_do_list.apps.ToDoListConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'to_do',
+        'USER': 'mauricio',
+        'PASSWORD': 'Bd_Connect',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configuration of static files
+STATICFILES_DIRS = [
+    PurePath(BASE_DIR, 'public')
+]
--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Items(models.Model):
+    content = models.CharField(max_length=1200)
+    status = models.BooleanField(default=False)
+
+
+# class ListItems(models.Model):
+#     id_user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
