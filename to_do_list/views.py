16f1b208f85b2c2cf74939ccb9846734f7712836
mauricioDosantos <mauriciodossantoscosta5@gmail.com>
2021-01-04 09:45:27 -0300

    configurado o desenpacotamento do forms

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -4,13 +4,16 @@ from .forms import ItemsForms
 
 def index(request):
     if request.method == 'POST':
-        form = ItemsForms(request.post)
+        form = ItemsForms(request.POST)
         if form.is_valid():
             form.save()
             print('Salvo com sucesso!')
         else:
+            print('Formulário inválido!')
+            print(form.errors)
             form = ItemsForms()  # tratamento de erro
     else:
         form = ItemsForms()
+        print('Não veio POST')
 
     return render(request, 'add-tarefa.html', {'form': form})
